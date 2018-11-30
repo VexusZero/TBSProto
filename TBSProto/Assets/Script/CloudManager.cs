283ec44c136e340bf436922ba90739630b39c119
Armonay <combustioncruz@gmail.com>
2018-11-30 02:07:25 -0300

    New Master
    Con los archivos de JP

---
+++ TBSProto/Assets/Script/CloudManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudManager : MonoBehaviour {
+
+    public GameObject cloudObject;
+
+    public GameObject[] spawns;
+
+    float currentTime = 0;
+
+    float waitTime = 5f;
+
+	// Use this for initialization
+	void Start () {
+       spawns = GameObject.FindGameObjectsWithTag("Spawn");
+
+        StartCoroutine(SpawnClouds());
+	}
+
+    void OnTriggerExit(Collider sensor)
+    {
+        if (sensor.transform.name == "Cloud")
+        {
+            print("boom");
+            GameObject.Destroy(sensor.transform.gameObject);
+            SpawnSingleCloud();
+        }
+    }
+
+    IEnumerator SpawnClouds()
+    {
+        int randomIndex = Random.Range(0, spawns.Length);
+
+        for (int i = 0; i < 6; i++) {
+         GameObject go = Instantiate(cloudObject, spawns[randomIndex].transform.position,Quaternion.identity) as GameObject;
+            yield return new WaitForSeconds(Random.Range(1,3));
+        }
+
+        yield return null;
+    }
+
+    void SpawnSingleCloud()
+    {
+        int randomIndex = Random.Range(0, spawns.Length);
+
+        GameObject go = GameObject.Instantiate(cloudObject, spawns[randomIndex].transform.position, Quaternion.identity) as GameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (currentTime > waitTime)
+        {
+            StartCoroutine(SpawnClouds());
+            currentTime = 0f;
+        }
+        currentTime += Time.deltaTime;
+	}
+}
---
+++ TBSProto/Assets/Script/CloudMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour {
+
+    float speed;
+    Vector3 vectorFactor;
+
+	// Use this for initialization
+	void Start () {
+        speed = Random.Range(0.05f, 0.1f);
+        vectorFactor = new Vector3(0f, 0f, speed);
+
+        GameObject.Destroy(gameObject, 15f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position += vectorFactor;
+	}
+}
